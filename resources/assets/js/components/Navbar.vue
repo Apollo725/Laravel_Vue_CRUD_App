94c57d0ddeef6209da272cb1e1423da949dfc2be
Apollo725 <natashazaharova725@outlook.com>
2018-08-25 15:39:06 +1000

    First Commit

---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="navbar navbar-expand-sm navbar-dark bg-info ms-2">
+    <div class="container">
+      <a href="#" class="navbar-brand">Larticles</a>
+    </div>
+  </nav>
+</template>
